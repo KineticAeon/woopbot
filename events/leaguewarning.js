bd2b0c28ac63c5d441f2239610a506bd3971172c
Aeon <aeoncaptain@gmail.com>
2022-10-19 20:24:06 -0600

    tells people to touch grass when they play league

---
+++ events/leaguewarning.js
@@ -0,0 +1,20 @@
+// val too maybe
+module.exports = {
+    name: 'presenceUpdate',
+    execute(oldPresence, newPresence, client) {
+
+        let channelID = 1032389840777510912
+        // shames the players for playing league
+        function shameLeaguePlayer(user) {
+            client.channels.cache.get(channelID).send(`<@${user}> should stop playing league and touch grass`)
+        }
+
+        // checks if a users presence has updated
+        if (Object.keys(newPresence.activities).length != 0) {
+            // sets activityName
+            let activityName = newPresence.activities[0].name.replace(/\s+/g, '').toLowerCase();
+            let user = newPresence.userID
+            if (activityName.includes('leagueoflegends')) shameLeaguePlayer(user);
+        }
+    },
+};
