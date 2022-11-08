bb3ccab4c6dcdcab21ddc4be78d6bb9438c2d999
Aeon <aeoncaptain@gmail.com>
2022-11-08 10:54:24 -0700

    league does two places now heheha

--- events/leaguewarning.js
+++ events/leaguewarning.js
@@ -3,9 +3,11 @@ module.exports = {
     execute(oldPresence, newPresence, client) {
 
         let channelID = '984322369273344050'
+        let channelID2 = '103959822629279070'
         // shames the players for playing league
         function shameLeaguePlayer(user) {
             client.channels.cache.get(channelID).send(`<@${user}> should stop playing league and touch grass`)
+            client.channels.cache.get(channelID2).send(`<@${user}> should stop playing league and touch grass`)
         }
 
         // checks if a users presence has updated
